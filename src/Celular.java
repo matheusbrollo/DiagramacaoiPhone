38eeb0d681cb791a18fe4c822b1695d8b53b472d
matheus <matheusbrollo201@gmail.com>
2025-04-15 23:09:43 -0300

    Diagramacao Concluida

---
+++ src/Celular.java
@@ -0,0 +1,5 @@
+public interface Celular {
+    void ligar(String numero);
+    void atender();
+    void iniciarCorreioVoz();
+}
---
+++ src/iPhone.java
@@ -0,0 +1,54 @@
+public class iPhone implements ReprodutorMusical, Celular, NavegadorInternet {
+    private String modelo;
+    private String sistemaOperacional;
+
+    public iPhone(String modelo, String sistemaOperacional) {
+        this.modelo = modelo;
+        this.sistemaOperacional = sistemaOperacional;
+    }
+
+    @Override
+    public void tocar() {
+        System.out.println("Tocando música...");
+    }
+
+    @Override
+    public void pausar() {
+        System.out.println("Música pausada");
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+        System.out.println("Selecionando música: " + musica);
+    }
+
+    @Override
+    public void ligar(String numero) {
+        System.out.println("Ligando para: " + numero);
+    }
+
+    @Override
+    public void atender() {
+        System.out.println("Atendendo chamada.");
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+        System.out.println("Iniciando correio de voz.");
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+        System.out.println("Exibindo página: " + url);
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+        System.out.println("Nova aba adicionada!");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        System.out.println("Página atualizada!");
+    }
+}
